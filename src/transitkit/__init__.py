7aec9e61acd3a9a66266adfe7f31598efdc19182
Arif Solmaz <arif.solmaz@gmail.com>
2026-01-14 17:55:30 +0300

    updates

--- src/transitkit/__init__.py
+++ src/transitkit/__init__.py
@@ -1,8 +1,8 @@
 """TransitKit: Exoplanet Transit Light Curve Analysis Toolkit"""
 
 __version__ = "0.1.0"
-__author__ = "YOUR NAME"
-__email__ = "YOUR@EMAIL.COM"
+__author__ = "Arif Solmaz"
+__email__ = "arif.solmaz@gmail.com"
 
 # You can start with just this
 print(f"TransitKit {__version__} loaded")
