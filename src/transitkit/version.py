4485ce4d2cb66669c02d186464680d98519236ca
Arif Solmaz <arif.solmaz@gmail.com>
2026-01-14 20:01:06 +0300

    Fix package metadata exports

--- src/transitkit/version.py
+++ src/transitkit/version.py
@@ -2,11 +2,11 @@
 
 __version__ = "1.0.0"
 __version_info__ = tuple(int(num) for num in __version__.split("."))
-__author__ = "Your Name"
-__email__ = "your.email@example.com"
+__author__ = "Arif Solmaz"
+__email__ = "arif.solmaz@gmail.com"
 __copyright__ = f"Copyright 2024 {__author__}"
 __license__ = "MIT"
-__url__ = "https://github.com/yourusername/transitkit"
+__url__ = "https://github.com/arifsolmaz/transitkit"
 __description__ = "Exoplanet Transit Light Curve Analysis Toolkit"
 __long_description__ = """
 TransitKit is a comprehensive, open-source software package for analyzing
